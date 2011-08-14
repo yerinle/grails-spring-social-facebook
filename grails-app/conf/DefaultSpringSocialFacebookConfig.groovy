02b26dc28b5dac856c829366932c01d89aee1ee0
Domingo Suarez Torres <domingo.suarez@gmail.com>
2011-08-14 04:52:36 -0500

    fixed view

--- grails-app/conf/DefaultSpringSocialFacebookConfig.groovy
+++ grails-app/conf/DefaultSpringSocialFacebookConfig.groovy
@@ -1,7 +1,7 @@
 springsocial {
     facebook.page.connect = "/springsocial/facebook/connect"
     facebook.page.profile = "/springsocial/facebook/index"
-	facebook.page.connectedHome = "/springsocial/facebook/index"
+	facebook.page.connectedHome = "/springSocialFacebook/index"
     facebook.page.feed = "/springsocial/facebook/feed"
     facebook.page.friends = "/springsocial/facebook/friends"
     facebook.page.albums = "/springsocial/facebook/albums"
