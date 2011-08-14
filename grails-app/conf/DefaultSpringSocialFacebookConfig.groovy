70193efce352d2ff4de868948e07ad242f273dbb
Domingo Suarez Torres <domingo.suarez@gmail.com>
2011-08-13 23:10:21 -0500

    added the connectedHome page

--- grails-app/conf/DefaultSpringSocialFacebookConfig.groovy
+++ grails-app/conf/DefaultSpringSocialFacebookConfig.groovy
@@ -1,6 +1,7 @@
 springsocial {
     facebook.page.connect = "/springsocial/facebook/connect"
     facebook.page.profile = "/springsocial/facebook/index"
+	facebook.page.connectedHome = "/springsocial/facebook/index"
     facebook.page.feed = "/springsocial/facebook/feed"
     facebook.page.friends = "/springsocial/facebook/friends"
     facebook.page.albums = "/springsocial/facebook/albums"
