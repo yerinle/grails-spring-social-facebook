d5835e902071e606433e800caca49c8ed2786555
Domingo Suarez Torres <domingo.suarez@gmail.com>
2011-08-17 18:51:01 -0500

    excluded JS files, setting version to 0.1.2

--- SpringSocialFacebookGrailsPlugin.groovy
+++ SpringSocialFacebookGrailsPlugin.groovy
@@ -1,13 +1,14 @@
 class SpringSocialFacebookGrailsPlugin {
     // the plugin version
-    def version = "0.1.1"
+    def version = "0.1.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
     def dependsOn = [springSocialCore: '0.1 > *']
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/views/error.gsp"
+            "grails-app/views/error.gsp",
+			"web-app/js"
     ]
 
     // TODO Fill in these fields
