8f9df77ee0f0f749fe37f8f71f4823338c82a71e
Domingo Suarez Torres <domingo.suarez@gmail.com>
2012-04-17 16:04:38 -0500

    upgraded to SpringSocialCore 0.1.33-SNAPSHOT

--- SpringSocialFacebookGrailsPlugin.groovy
+++ SpringSocialFacebookGrailsPlugin.groovy
@@ -1,13 +1,32 @@
+/* Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 class SpringSocialFacebookGrailsPlugin {
-  def version = "0.1.32"
+  def version = "0.1.33-SNAPSHOT"
   def grailsVersion = "1.3 > *"
-  def dependsOn = [springSocialCore: '0.1.31 > *']
+  def dependsOn = [:]
   // resources that are excluded from plugin packaging
   def pluginExcludes = [
       "grails-app/views/error.gsp",
       "web-app/js",
       "web-app/images/**",
-      "web-app/images/skin/**"
+      "web-app/images/skin/**",
+      "grails-app/conf/Config.groovy",
+      "grails-app/conf/DataSource.groovy",
+      "grails-app/i18n/**",
+      'docs/**',
+      'src/docs/**'
   ]
 
   // TODO Fill in these fields
@@ -18,6 +37,13 @@ class SpringSocialFacebookGrailsPlugin {
 
   // URL to the plugin's documentation
   def documentation = "http://grails.org/plugin/spring-social-facebook"
+
+  def license = "APACHE"
+  def organization = [name: "Sindicato Source", url: "http://sindica.to"]
+  def developers = [[name: "Domingo Suarez Torres", email: "domingo.suarez@gmail.com"]]
+  def scm = [url: "https://github.com/synergyj/grails-spring-social-facebook"]
+  def issueManagement = [system: "GITHUB", url: "https://github.com/synergyj/grails-spring-social-facebook/issues"]
+
 
   def doWithSpring = {
     xmlns context: "http://www.springframework.org/schema/context"
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,4 +1,4 @@
-/* Copyright 2011 the original author or authors.
+/* Copyright 2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,9 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-def springSocialVersion = "1.0.0.RELEASE"
-
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
@@ -39,12 +36,39 @@ grails.project.dependency.resolution = {
     mavenRepo "http://maven.springframework.org/release"
     mavenRepo "http://maven.springframework.org/snapshot"
     mavenRepo "http://maven.springframework.org/milestone"
+
+    mavenRepo "http://repo.clickonero.com/nexus/content/repositories/snapshots/"
+    grailsRepo "http://grails.org/plugins"
+
+    grailsRepo "http://repo.clickonero.com/nexus/content/repositories/snapshots/"
   }
   dependencies {
+    def springSocialVersion = "1.0.1.RELEASE"
     compile("org.springframework.social:spring-social-facebook:${springSocialVersion}") { transitive = false }
+
+    compile("org.codehaus.jackson:jackson-mapper-asl:1.9.2")
   }
   plugins {
-    compile(":spring-security-core:1.2.4")
-    compile(":spring-social-core:0.1.31")
+    compile(":spring-social-core:0.1.33-SNAPSHOT")
+
+    compile(":spring-security-core:1.2.7.3")
+    test(":code-coverage:1.2.5") { export = false }
+    test("org.spockframework:spock:0.6-groovy-1.8-SNAPSHOT") { export = false }
+    build(":release:2.0.0") { export = false }
+    build(":rest-client-builder:1.0.2") { export = false }
   }
+}
+
+grails.project.repos.clickonero.url = "http://repo.clickonero.com/nexus/content/repositories/snapshots/"
+
+grails.release.scm.enabled = false
+//grails.project.repos.default = "grailsCentral"
+grails.project.repos.default = "clickonero"
+
+coverage {
+  exclusions = [
+      "DefaultSpringSocialConfig*",
+      "SpringSocialCoreDefaultConfig*"
+  ]
+  enabledByDefault = true
 }
